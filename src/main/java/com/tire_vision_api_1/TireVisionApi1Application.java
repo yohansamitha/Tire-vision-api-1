41236f568975f7569d6a46f2ff9e9a148ad6a218
YohanSamitha <yohansamitha123@gmail.com>
2024-09-05 15:07:03 +0530

    init commit

---
+++ src/main/java/com/tire_vision_api_1/TireVisionApi1Application.java
@@ -0,0 +1,13 @@
+package com.tire_vision_api_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireVisionApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireVisionApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tire_vision_api_1/TireVisionApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tire_vision_api_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireVisionApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
